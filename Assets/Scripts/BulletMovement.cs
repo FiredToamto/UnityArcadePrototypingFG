3712f51b76740c8e407ebe27da348d93a4062474
Theo <theo.d.tolstoy@gmail.com>
2020-10-12 23:30:05 +0200

    !A bullet movement logic

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BulletMovement : MonoBehaviour
+{
+    public Transform bulletOwner;
+    public float bulletSpeed = 10f;
+
+    private Transform _tr;
+    void Awake()
+    {
+        _tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _placeHolderMethod();
+    }
+
+    private void _placeHolderMethod() 
+    {
+        if (Input.anyKey)
+        {
+            _tr.position = Vector3.MoveTowards(_tr.position, bulletOwner.position, Time.deltaTime * bulletSpeed);
+        }
+    }
+}
