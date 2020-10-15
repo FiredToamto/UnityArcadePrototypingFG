6f3021a2af6dfbff67c744e07449d74d6a3a3dd0
Theo <theo.d.tolstoy@gmail.com>
2020-10-15 16:54:21 +0200

    Fixed camera and some flipping problems

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour{
+    public Transform target;
+    [Tooltip("Seconds to reach target")] public float smoothTime = 0.5f;
+    public Vector3 offset = new Vector3(0f, 0f, -10f);
+
+    private Vector3 _velocity;
+    private Transform _transform;
+
+    private void Start(){
+        _transform = transform;
+        _transform.position = target.position + offset;
+    }
+
+    private void FixedUpdate(){
+        Vector3 targetPosition = target.TransformPoint(offset);
+        _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _velocity, smoothTime);
+    }
+}
