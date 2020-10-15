d92821a33e80aecd76c381ef85b0bacf62fe5f3d
Slume <61590616+Slumee@users.noreply.github.com>
2020-10-15 10:58:51 +0200

    Movement system o sånt,  flytt från local projekt

---
+++ Assets/Scripts/GhostTrail.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class GhostTrail : MonoBehaviour
+{
+    public float ghostDelay;
+    private float ghostDelaySeconds;
+    public GameObject ghost;
+    public bool makeGhost = true;
+
+    void Start()
+    {
+        ghostDelaySeconds = ghostDelay;
+
+    }
+
+    void Update()
+    {   
+            // Checks if player is moving to generate trail
+
+
+        
+        if (ghostDelaySeconds > 0)
+        {   // Time since last frame
+            ghostDelaySeconds -= Time.deltaTime;
+        }
+        else
+        {
+            //Generate a ghost
+            GameObject currentGhost = Instantiate(ghost, transform.position, transform.rotation);
+
+            //Sets the ghost trail to mimic current sprite frame
+            currentGhost.transform.localRotation = this.transform.localRotation;
+
+           ghostDelaySeconds = ghostDelay;
+
+            //Destroys ghost after set delay
+            Destroy(currentGhost, 1f);
+        }
+    }
+}
