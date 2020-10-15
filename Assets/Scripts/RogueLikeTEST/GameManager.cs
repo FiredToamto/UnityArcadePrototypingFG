cf00b69d690042629eaa06b24da79e316e2be8ff
WingmanAlex <a.bergendorff.r@gmail.com>
2020-10-15 12:02:33 +0200

    1stPush
    A! RogueLike tile system. Game manager, boardmanager ´+ placeholder assets

---
+++ Assets/Scripts/RogueLikeTEST/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager
+        instance = null;
+
+    private BoardManager _boardScript;
+    private int level = 3;
+    
+    void start()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        _boardScript = GetComponent<BoardManager>();
+
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        _boardScript.SetupScene(level);
+
+    }
+    
+    void Update()
+    {
+
+    }
+
+}    
