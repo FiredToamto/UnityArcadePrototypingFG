642d70ee03bdfc2fec008e67f9f87cc27dcab211
Theo <theo.d.tolstoy@gmail.com>
2020-10-15 10:45:58 +0200

    Backgrounds and stuff

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,6 +2,7 @@
 
 public class EnemyMovement : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    
+    private Vector2 _offset;
+    private Material _myMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myMat = GetComponent<Renderer>().material;
+        _offset = new Vector2(0, scrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _myMat.mainTextureOffset += _offset * Time.deltaTime;
+    }
+}
