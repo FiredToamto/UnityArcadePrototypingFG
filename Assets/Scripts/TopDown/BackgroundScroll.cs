1594ff7f307c42386876eba2dfa9c6736f195d83
WingmanAlex <a.bergendorff.r@gmail.com>
2020-10-19 15:04:22 +0200

    Scrolling Background
    2 versions of scrolling background

---
+++ Assets/Scripts/TopDown/BackgroundScroll.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace TopDown
+{
+    public class BackgroundScroll : MonoBehaviour
+    {
+        private Material _material;
+        private Vector2 _offset;
+
+        public float xVelocity, yVelocity;
+        
+        
+        private void Awake()
+        {
+            //Acces to material on game object
+            _material = GetComponent<Renderer>().material;
+        }
+
+        void Start()
+        {    
+            // create new offset based on velocity
+            //_offset = new Vector2(xVelocity, yVelocity);
+        }
+        
+        void Update()
+        {
+            // create new offset based on velocity
+            // access to offset
+            _offset = new Vector2(xVelocity, yVelocity);
+            _material.mainTextureOffset += _offset * Time.deltaTime;
+
+
+        }
+    }
+}
--- Assets/Scripts/TopDown/PlayerMovement.cs
+++ Assets/Scripts/TopDown/PlayerMovement.cs
@@ -5,7 +5,7 @@ namespace TopDown
 {
     [Serializable]
     public class Boundary
-    {
+    {  //Serialized float for movement boundaries 
         public float xMin, xMax, yMin, yMax;
     }
 
@@ -15,14 +15,21 @@ namespace TopDown
         public Rigidbody2D rb;
         public Boundary boundary;
         public float tilt;
+        public Camera cam;
 
         private Vector2 _movement;
+        private Vector2 _mousePosition;
         
         void Update()
         {
+            //input from Input Manager
             _movement.x = Input.GetAxisRaw("Horizontal");
             _movement.y = Input.GetAxisRaw("Vertical");
             
+            //Calculate where mouse is from pixels to world pos.
+            _mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            
+            //Create a clamped boundary where the player can or cannot move
             rb.position = new Vector3
                 
             (
@@ -35,39 +42,56 @@ namespace TopDown
         }
 
         void FixedUpdate()
-        {
+        { 
+            //Rigidbody movement calculation
             rb.MovePosition(rb.position + _movement * moveSpeed * Time.fixedDeltaTime);
+            
+           
+            
+            //MOUSE AIMING
+            //calculate mouse direction from player to mouse.
+            Vector2 lookDir = _mousePosition - rb.position;
+            //Return y,x angle from mouse direction. convert radians to degrees.
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+            //Apply to player
+            rb.rotation = angle;
+            
+
+            
+           
+           //test of tilt....
             //rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
-            
-           /*
-            
-            Testing tilt stuff
-            
-            
-            //Move the controller in that direction
-            controller.MovePosition (transform.position + (inputVec) * Time.deltaTime);
- 
-            //Determines the level of tilt based on user input
-            if (Mathf.Abs (vertical) > 0 && Mathf.Abs (horizontal) > 0) 
-            {
-                tiltAngle = 1.5f;
-            } 
-            else 
-            {
-                tiltAngle = 3f;
-            }
- 
-            //Calculates the tilt based on user input
-            
-            Quaternion target = Quaternion.Euler ((Mathf.Abs(vertical) + Mathf.Abs(horizontal)) * tiltAngle, 0, 0);
- 
-            if (inputVec != Vector3.zero) 
-            {
-                //Smoothing rotates the character and assigns the new rotation to CharacterRotation
-                characterRotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (inputVec, Vector3.up), Time.deltaTime * smooth);
-                transform.rotation = characterRotation * target;
-            }
-           */
+
+           
+            /*
+             
+             Testing tilt stuff
+             
+             
+             //Move the controller in that direction
+             controller.MovePosition (transform.position + (inputVec) * Time.deltaTime);
+  
+             //Determines the level of tilt based on user input
+             if (Mathf.Abs (vertical) > 0 && Mathf.Abs (horizontal) > 0) 
+             {
+                 tiltAngle = 1.5f;
+             } 
+             else 
+             {
+                 tiltAngle = 3f;
+             }
+  
+             //Calculates the tilt based on user input
+             
+             Quaternion target = Quaternion.Euler ((Mathf.Abs(vertical) + Mathf.Abs(horizontal)) * tiltAngle, 0, 0);
+  
+             if (inputVec != Vector3.zero) 
+             {
+                 //Smoothing rotates the character and assigns the new rotation to CharacterRotation
+                 characterRotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (inputVec, Vector3.up), Time.deltaTime * smooth);
+                 transform.rotation = characterRotation * target;
+             }
+            */
         }
     }
 }
