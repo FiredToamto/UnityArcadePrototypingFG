829edb30616fb87b258977c2039e7ab1f9b06c71
Theo <theo.d.tolstoy@gmail.com>
2020-10-21 15:17:31 +0200

    new stuff

---
+++ Assets/Scripts/bulletMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletMethods : MonoBehaviour
+{
+    public void bulletExplode()
+    {
+        //partilce effects
+        //kills/destroys enemy or player when triggered
+        //kills/destroys itself
+    }
+    public void deflect()
+    {
+        //deflect bullet back to enemy
+    }
+    public void disableVelocity()
+    {
+
+    }
+    public void disableHitDetection()
+    {
+
+    }
+}
