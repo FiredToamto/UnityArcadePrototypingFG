3f63e67cd176178a127299d30ca1422bbc7cb20e
WingmanAlex <a.bergendorff.r@gmail.com>
2020-10-22 12:34:14 +0200

    asdsadas

---
+++ Assets/Scripts/AlexTEST/CheckpointController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+
+    public Sprite flagClosed;
+    public Sprite flagOpen;
+
+    private SpriteRenderer theSpriteRenderer;
+
+    public bool checkpointActive;
+
+    void Start()
+    {
+        theSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+        
+    void Update()
+    {
+        
+    }
+    // Has the player passed through check point. If 'Player' change sprite. 
+    // show if active or not.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            theSpriteRenderer.sprite = flagOpen;
+            checkpointActive = true;
+        }
+    }
+
+}
