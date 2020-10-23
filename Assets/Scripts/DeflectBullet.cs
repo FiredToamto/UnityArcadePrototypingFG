4439ca8491bb625f27e556916f8819ba20b1fe5d
Theo <theo.d.tolstoy@gmail.com>
2020-10-23 14:01:20 +0200

    new stuff

--- Assets/Scripts/DeflectBullet.cs
+++ Assets/Scripts/DeflectBullet.cs
@@ -2,14 +2,13 @@
 
 public class DeflectBullet : MonoBehaviour
 {
-    public int shieldType = 1;
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        IInteraction bullet = coll.GetComponent<BulletMovement>();
-        Rigidbody2D rbBullet = coll.GetComponent<Rigidbody2D>();
-        if (bullet != null)
+        if (coll.gameObject.CompareTag("Bullet"))
         {
-            bullet.Interact(shieldType);
+            BulletMethods bulletM = coll.GetComponent<BulletMethods>();
+            bulletM.DisableVelocity();
+            bulletM.Deflected();
         }
     }
 }
