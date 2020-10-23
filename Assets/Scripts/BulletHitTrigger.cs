27dba45e16743cc36168599b51d729d41f559688
Theo <theo.d.tolstoy@gmail.com>
2020-10-23 15:33:11 +0200

    new stuff

--- Assets/Scripts/BulletHitTrigger.cs
+++ Assets/Scripts/BulletHitTrigger.cs
@@ -4,34 +4,26 @@ using System.Collections.Generic;
 
 public class BulletHitTrigger : MonoBehaviour
 {
-    private GameObject bullet;
-    private void Awake()
-    {
-        bullet = gameObject;
-    }
-
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("killed or hurt player");
-            coll.gameObject.GetComponent<Respawner>().Respawn();
-            bullet.SetActive(false);
-
-        }
         if (coll.gameObject.CompareTag("Enemy"))
         {
-            if (bullet.GetComponent<BulletMovement>().deflected)
+            if (GetComponent<BulletMoveBackToOwner>().deflected)
             {
-                Debug.Log("killed or hurt enemy");
                 coll.gameObject.SetActive(false);
-                bullet.SetActive(false);
+                gameObject.SetActive(false);
             }
+        }
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            GetComponent<BulletMethods>().BulletExplode();
+        }
+        else
+        {
+            GetComponent<BulletMethods>().BulletExplode();
+
         }
 
     }
 
-    private void OnCollisionEnter2D(Collision2D coll)
-    {
-    }
 }
---
+++ Assets/Scripts/EnemyCannon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCannon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float shootIntervall = 10f;
+    public float bulletSpeed = 10f;
+    public Vector3 bulletSize;
+    public SpriteRenderer sR;
+
+    private Transform _tr;
+    private Transform playerPos;
+    private IEnumerator coroutine;
+    private void Awake()
+    {
+        _tr = GetComponent<Transform>();
+    }
+    private void Start()
+    {
+        coroutine = ShootTimer(shootIntervall);
+        Shoot();
+        StartCoroutine(coroutine);
+    }
+    private IEnumerator ShootTimer(float intervall)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(intervall);
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        GameObject newBullet = Instantiate(bulletPrefab, gameObject.transform.position, gameObject.transform.rotation);
+        newBullet.GetComponent<Transform>().localScale = bulletSize;
+        newBullet.GetComponent<SpriteRenderer>().color = sR.color;
+        newBullet.GetComponent<BulletMoveBackToOwner>().bulletOwner = _tr;
+        newBullet.GetComponent<Rigidbody2D>().velocity = transform.right * bulletSpeed;
+    }
+}
